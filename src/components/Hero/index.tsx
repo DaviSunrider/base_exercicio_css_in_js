9a575c6ed96ae78f5c7cced4e0f7f7aaa077309f
DaviSunrider <115567218+DaviSunrider@users.noreply.github.com>
2024-02-16 19:03:41 -0300

    add styled components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,11 @@
-import styles from './Hero.module.css'
+import { Conteudo, Formulario, H2 } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <Formulario>
+    <Conteudo>
+      <H2>As melhores vagas para tecnologia, design e artes visuais.</H2>
+    </Conteudo>
+  </Formulario>
 )
 
 export default Hero
